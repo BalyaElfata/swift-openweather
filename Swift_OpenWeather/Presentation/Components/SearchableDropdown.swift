32a3c41321df06418c70093fcebcfa804b13a9c6
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 13:59:07 +0700

    fix: added navigationstack back to handle searchable data error

--- Swift_OpenWeather/Presentation/Components/SearchableDropdown.swift
+++ Swift_OpenWeather/Presentation/Components/SearchableDropdown.swift
@@ -7,32 +7,36 @@ struct SearchableDropdown: View {
     let options: [String]
 
     var body: some View {
-        VStack {
-            if networkManager.isConnected == false {
-                ContentUnavailableView("Network Error", systemImage: "wifi.slash", description: Text("Koneksi Error. Cek kembali koneksi anda dan ulangi."))
-            } else if $viewModel.provinces.isEmpty {
-                ContentUnavailableView("API Error", systemImage: "wifi.exclamationmark", description: Text("Error mengambil data \(type == .province ? "provinsi" : "kota") dari API."))
-            } else {
-                List {
-                    ForEach(searchResults, id: \.self) { option in
-                        Button {
-                            if type == .province {
-                                viewModel.selectedProvince = option
-                                viewModel.provinceCode = viewModel.provinces.first(where: { $0.name == viewModel.selectedProvince })?.code ?? ""
-                                viewModel.isSelectingProvince = false
-                            } else {
-                                viewModel.selectedCity = String(option.dropFirst(5))
-                                viewModel.isSelectingCity = false
+        NavigationStack {
+            VStack {
+                if networkManager.isConnected == false {
+                    ContentUnavailableView("Network Error", systemImage: "wifi.slash", description: Text("Koneksi Error. Cek kembali koneksi anda dan ulangi."))
+                } else if $viewModel.provinces.isEmpty {
+                    ContentUnavailableView("API Error", systemImage: "wifi.exclamationmark", description: Text("Error mengambil data \(type == .province ? "provinsi" : "kota") dari API."))
+                } else if searchResults.isEmpty {
+                    ContentUnavailableView("Tidak Ada Hasil Pencarian", systemImage: "magnifyingglass", description: Text("Cek ejaan dari \(type == .province ? "provinsi" : "kota") atau coba pencarian baru."))
+                } else {
+                    List {
+                        ForEach(searchResults, id: \.self) { option in
+                            Button {
+                                if type == .province {
+                                    viewModel.selectedProvince = option
+                                    viewModel.provinceCode = viewModel.provinces.first(where: { $0.name == viewModel.selectedProvince })?.code ?? ""
+                                    viewModel.isSelectingProvince = false
+                                } else {
+                                    viewModel.selectedCity = String(option.dropFirst(5))
+                                    viewModel.isSelectingCity = false
+                                }
+                            } label: {
+                                Text(option)
+                                    .foregroundStyle(.black)
                             }
-                        } label: {
-                            Text(option)
-                                .foregroundStyle(.black)
                         }
                     }
                 }
             }
+            .searchable(text: $viewModel.searchText)
         }
-        .searchable(text: $viewModel.searchText)
     }
 
     var searchResults: [String] {
@@ -45,9 +49,7 @@ struct SearchableDropdown: View {
 }
 
 #Preview {
-    NavigationStack {
-        SearchableDropdown(type: .province, options: ["Holly", "Josh", "Rhonda", "Ted"])
-            .environmentObject(FormViewModel())
-            .environmentObject(NetworkManager())
-    }
+    SearchableDropdown(type: .province, options: ["Holly", "Josh", "Rhonda", "Ted"])
+        .environmentObject(FormViewModel())
+        .environmentObject(NetworkManager())
 }
