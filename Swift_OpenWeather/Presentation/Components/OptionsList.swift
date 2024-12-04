fdfbbd08eb0379699ee9bca6f41b8877e3b43afa
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 11:42:36 +0700

    feat: added OptionsList file for alternative searchabledropdown

---
+++ Swift_OpenWeather/Presentation/Components/OptionsList.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsList.swift
+//  Swift_OpenWeather
+//
+//  Created by Balya Elfata on 04/12/24.
+//
+
+import SwiftUI
+
+struct OptionsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsList()
+}
